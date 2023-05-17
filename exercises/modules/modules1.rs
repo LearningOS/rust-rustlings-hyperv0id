2aa97b58df260a50c8f66d27dddd4584cd9a09cc
jcheng <20722003@bjtu.edu.cn>
2023-05-17 15:07:01 +0800

    modules

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -1,15 +1,15 @@
 // modules1.rs
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    // pub: 表示该项是公共的，可以从任何地方访问。
+    // crate: 表示可见性的作用域被限制在当前 crate 内。
+    pub(crate) fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
